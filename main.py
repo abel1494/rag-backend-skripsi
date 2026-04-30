f831a09d1a0e7fdafe48529f0a11ab9c8a2a38e9
abel1494 <118557596+abel1494@users.noreply.github.com>
2026-04-30 23:12:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -34,10 +34,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://skripsi-rag-academic-assistent-syst.vercel.app",
-        "http://localhost:3000"
-    ], 
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
